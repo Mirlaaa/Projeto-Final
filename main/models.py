afda831dd9d35c896ea77b543505ee9186a6e9ec
laragiiovana <pereiralara958@gmail.com>
2022-12-12 21:13:34 -0300

    criação dos models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.EmailField()
+    senha = models.IntegerField()
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=150)
+    peso = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
+    descricao = models.TextField()
+    # imagem
+
+class Plano(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    valor = models.IntegerField()
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    # imagem
+    
+class Treinador(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.EmailField()
+    telefone = models.IntegerField()
+    especializacao = models.CharField(max_length=100)
+    # imagem
+    # link
+
+class Postagem(models.Model):
+    titulo = models.CharField(max_length=150)
+    autor = models.CharField(max_length=100)
+    data_postagem = models.DateField()
+    descricao = models.TextField()
+    # imagem  
+  
